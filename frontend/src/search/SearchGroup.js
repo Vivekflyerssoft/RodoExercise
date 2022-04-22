01b5be320705c8248821808d3fed796f41f0f1df
Vivek <vivek.n@flyerssoft.com>
2022-04-22 22:23:52 +0530

    Adding more prices and moving it constants

--- frontend/src/search/SearchGroup.js
+++ frontend/src/search/SearchGroup.js
@@ -8,6 +8,7 @@ export const SearchGroup = ({ searchDataChanged }) => {
         year: 'year',
         price: 'price'
     }
+    const PRICES_AVAILABLE = [2000, 4000, 6000, 8000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 60000, 70000, 80000, 90000, 100000, 125000, 150000, 175000];
 
     const [makeAndModelsList, setMakeAndModelList] = useState([]);
     const [makeList, setMakeList] = useState([]);
@@ -75,7 +76,7 @@ export const SearchGroup = ({ searchDataChanged }) => {
                 title="Price"
                 uId={FIELDS.price}
                 option_zero="No max price"
-                data={[2000, 4000, 6000, 8000]}
+                data={PRICES_AVAILABLE}
                 onSelectOptionChanged={(value) => setSelectedPrice(value)} />
         </div>
     )
